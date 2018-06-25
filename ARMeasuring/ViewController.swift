8892a536e9df4fdded62fa6a2e593407d17cd43b
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-25 13:54:01 +0900

    Added aiming icon. Beginning point for measuring using matrix transformations for point of view and start position of camera

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        var transalationMatrix = matrix_identity_float4x4
+        transalationMatrix.columns.3.z = -0.1
+        var modifiedMatrix = simd_mul(transform, transalationMatrix)
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = modifiedMatrix
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+    }
 
 }
 
